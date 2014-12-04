28e4a7accfd5c594fa43fbd2d2bc72078fc90ece
mcurrr <technosigal@gmail.com>
2014-12-04 23:30:08 +0200

    add game thing

---
+++ game/game.js
@@ -0,0 +1,31 @@
+var Game = require ("gameloop-canvas");
+var Keyboard = require ("crtrdg-keyboard");
+var Mouse = require ("crtrdg-mouse");
+
+var Player = require("./player.js");
+var Bullet = require("./bullet.js");
+var Enemy = require("./enemy.js");
+
+var game = Game ({
+	canvas: "game",
+	width: window.innerWidth,
+	height: window.innerHeight
+});
+
+var keyboard = new Keyboard(game);
+var mouse = new Mouse(game);
+
+mouse.on("click", function(e) {});
+
+game.on("update", function(dt) {
+
+});
+
+game.on("draw", function(context) {
+	context.fillStyle = "#ef4e12";
+	context.fillRect(0, 0, game.width, game.height);
+});
+
+var player = new Player (game, {keys: keyboard.keysDown});
+
+game.start();
---
+++ game/player.js
@@ -0,0 +1,80 @@
+var Entity = require ("crtdrg-entity");
+var aabb = require ("aabb-2d");
+var inherits = require ("inherits");
+
+module.exports = Player;
+inherits (Player, Entity);
+
+function Player (game, options) {
+	var self = this;
+
+	this.game = game;
+	this.addTo (game);
+	this.keys = options.keys;
+	this.width = 20;
+	this.height = 20;
+	this.x = game.width / 2;
+	this.y = game.height / 2;
+	this.color = "#ffffff";
+	this.speed = 10;
+	this.friction = 0.9;
+
+	this.velocity = {
+		x: 0,
+		y: 0
+	};
+
+	this.boundingBox = aabb ([this.x, this.y] [this.width, this.height]);
+
+	this.game.on("update", function (dt) {
+		if (self.exist) {
+			self.input();
+			self.move();
+			self.boundaries();
+			self.boundingBox = aabb([self.x, self.y], [self.width, self.height]);
+		}
+	});
+};
+
+Player.prototype.boundaries = function () {
+	this.x += this.velocity.x;
+	this.y += this.velocity.y;
+	this.velocity.x *= this.friction;
+	this.velocity.y *= this.friction;
+};
+
+Player.prototype.boundaries = function () {
+	if (this.x <= 0) {
+		this.x = 0
+	}
+
+	if (this.y <= 0) {
+		this.y = 0
+	}
+
+	if (this.x >= this.game.width - this.width) {
+		this.x = this.game.width - this.width
+	}
+
+	if (this.y >= this.game.height - this.height) {
+		this.x = this.game.height - this.height
+	}
+};
+
+Player.prototype.input = function () {
+	if ("A" in this.keys) {
+		this.velocity.x = -this.speed;
+	}
+
+	if ("D" in this.keys) {
+		this.velocity.x = this.speed;
+	}
+
+	if ("W" in this.keys) {
+		this.velocity.y = -this.speed;
+	}
+
+	if ("S" in this.keys) {
+		this.velocity.y = this.speed;
+	}
+};
