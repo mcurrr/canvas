cd536f49b78eea52daa081b213b58c8bebaee4e3
mcurrr <technosigal@gmail.com>
2015-03-20 23:17:55 +0200

    gulp

---
+++ game/gulpfile.js
@@ -0,0 +1,27 @@
+var gulp = require ('gulp'),
+	uglify = require('gulp-uglify'),
+	rename = require('gulp-rename'),
+	plumber = require('gulp-plumber'),
+	livereload = require('gulp-livereload'),
+	source = require('vinyl-source-stream'),
+	browserify = require('browserify');
+
+gulp.task('scripts', function() {
+	return browserify('./js/game.js')
+			.bundle()
+			.pipe(source('all.js'))
+			.pipe(gulp.dest('build/'))
+			.pipe(livereload());
+});
+
+gulp.task('watch', function() {
+
+	gulp.watch('js/*.js', ['scripts']);
+
+	livereload.listen();
+
+	gulp.watch('*').on('change', livereload.changed);
+	gulp.watch('*/*').on('change', livereload.changed);
+});
+
+gulp.task('default', ['scripts', 'watch']);
